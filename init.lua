39c9946d74590c0e1158ddc490141cc0c72d3ace
Austin Matzko <austin@pressedcode.com>
2024-10-25 12:30:02 -0400

    Add an icon picker

--- init.lua
+++ init.lua
@@ -248,6 +248,19 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'ziontee113/icon-picker.nvim',
+    config = function()
+      require('icon-picker').setup { disable_legacy_commands = true }
+
+      local opts = { noremap = true, silent = true }
+
+      vim.keymap.set('n', '<Leader><Leader>i', '<cmd>IconPickerNormal<cr>', opts)
+      vim.keymap.set('n', '<Leader><Leader>y', '<cmd>IconPickerYank<cr>', opts) --> Yank the selected icon into register
+      vim.keymap.set('i', '<C-i>', '<cmd>IconPickerInsert<cr>', opts)
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
