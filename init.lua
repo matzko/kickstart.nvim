d2c9222c39cfd78614e973af613ae46acddf2690
Austin Matzko <austin@pressedcode.com>
2024-10-30 18:14:10 -0400

    Use ufo for handling folding.

--- init.lua
+++ init.lua
@@ -250,6 +250,38 @@ require('lazy').setup({
     },
   },
   'github/copilot.vim',
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+      --   -- Option 2: nvim lsp as LSP client
+      --   -- Tell the server the capability of foldingRange,
+      --   -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+      --   local capabilities = vim.lsp.protocol.make_client_capabilities()
+      --   capabilities.textDocument.foldingRange = {
+      --     dynamicRegistration = false,
+      --     lineFoldingOnly = true,
+      --   }
+      --   local language_servers = require('lspconfig').util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      --   for _, ls in ipairs(language_servers) do
+      --     require('lspconfig')[ls].setup {
+      --       capabilities = capabilities,
+      --       -- you can add other fields for setting up lsp server in this table
+      --     }
+      --   end
+      --   require('ufo').setup()
+
+      -- Option 3: treesitter as a main provider instead
+      -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+      -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+      -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      }
+    end,
+  },
   'tpope/vim-endwise',
   'tpope/vim-fugitive',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
