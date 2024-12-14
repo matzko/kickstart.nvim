c7b81a62a4da6c961239adac70e0fce975fa3894
Austin Matzko <austin@pressedcode.com>
2024-12-14 16:02:32 -0500

    More prettier settings

--- init.lua
+++ init.lua
@@ -823,7 +823,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        css = { 'prettier', stop_after_first = true },
+        scss = { 'prettier', stop_after_first = true },
+        -- sass = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
